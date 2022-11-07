85adce6ecf11bbc8fcd9829feb274ef78180e573
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2022-11-07 11:54:01 +0100

    dev: added first implementation with local data.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
+import { Dashboard, DefaultDataProvider } from "superset-dashboard";
+
 import Layout from "../components/Layout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+const dataProvider = new DefaultDataProvider("http://localhost:8088", {
+  username: "guest",
+  password: "guest",
+});
 
 const Index = () => {
   return (
     <Layout fullwidth>
-      <h1>Hello, Template!</h1>
+      <Dashboard
+        id="980cabb2-8a5b-434f-831b-469dd53172e5"
+        domain="http://localhost:8088"
+        dataProvider={dataProvider}
+      />
     </Layout>
   );
 };
