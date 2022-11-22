1ef88a526b2d74b4ad5b78957c0b2ef784dc21cb
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2022-11-22 18:48:42 +0100

    fix: removed invalid import

--- pages/dashboard/[slug].tsx
+++ pages/dashboard/[slug].tsx
@@ -1,15 +1,12 @@
-import { useEffect, useMemo, useState } from "react";
-
 import Breadcrumbs from "components/Breadcrumbs";
-import { Dashboard } from "superset-dashboard-sdk/build/DataProvider.types";
 import { Dashboard as EmbeddedDashboard } from "superset-dashboard-sdk";
 import { Layout } from "components";
 import { SUPERSET_DOMAIN } from "config";
 import Spinner from "components/Spinner";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useDashboard } from "components/Superset";
+import { useMemo } from "react";
 import { useRouter } from "next/router";
-import { useSupersetContext } from "components/Superset/SupersetContext";
 
 export default () => {
   const router = useRouter();
