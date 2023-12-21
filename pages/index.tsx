cd328c456aef9f4f804ed0c850b88f234b83bbe1
ITS-MATERA <eleonora.forcillo@itsmt.it>
2023-12-21 16:26:52 +0100

    fix: layout card auth

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,15 @@ import { SUPERSET_DOMAIN, configProvider, dataProvider } from "../config";
 import { Dashboard } from "superset-dashboard-sdk";
 import Layout from "../components/Layout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useState } from "react"; // new line
+import { useState } from "react";
 
 const Index = () => {
   const homeDashboard = configProvider.getHomePageDashboardConfig();
-  const [password, setPassword] = useState(""); // new line
-  const [showDashboard, setShowDashboard] = useState(false); // new line
-  const [error, setError] = useState(""); // new line
+  const [password, setPassword] = useState("");
+  const [showDashboard, setShowDashboard] = useState(false);
+  const [error, setError] = useState("");
 
   const handlePasswordSubmit = () => {
-    //new function
     const correctPassword = "AReSSits2023!";
     if (password === correctPassword) {
       setShowDashboard(true);
@@ -23,25 +22,52 @@ const Index = () => {
 
   return (
     <Layout fullwidth>
-      {!showDashboard ? ( // new condition for true
-        <div>
-          <h2>Inserisci la password per accedere</h2>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        {!showDashboard ? (
+          <div
+            style={{
+              width: "300px",
+              padding: "20px",
+              border: "1px solid #660b0e",
+              borderRadius: "5px",
+              textAlign: "center",
+            }}
+          >
+            <h2>Inserisci la password per accedere</h2>
+            <input
+              style={{ border: "1px solid #660b0e", borderRadius: "2px" }}
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              style={{
+                background: "#660b0e",
+                color: "white",
+                borderRadius: "2px",
+              }}
+              onClick={handlePasswordSubmit}
+            >
+              Accedi
+            </button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+          </div>
+        ) : (
+          <Dashboard
+            uuid={homeDashboard.uuid}
+            domain={SUPERSET_DOMAIN}
+            dataProvider={dataProvider}
+            fullheight
           />
-          <button onClick={handlePasswordSubmit}>Accedi</button>
-          {error && <p style={{ color: "red" }}>{error}</p>}
-        </div>
-      ) : (
-        <Dashboard
-          uuid={homeDashboard.uuid}
-          domain={SUPERSET_DOMAIN}
-          dataProvider={dataProvider}
-          fullheight
-        />
-      )}
+        )}
+      </div>
     </Layout>
   );
 };
