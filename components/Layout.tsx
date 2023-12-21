bb3803db0b94b672b3c8d11b833b57b7846b6a46
ITS-MATERA <eleonora.forcillo@itsmt.it>
2023-12-21 17:18:01 +0100

    dev: layout card auth 2

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,6 +7,7 @@ import Script from "next/script";
 import classNames from "classnames";
 import classes from "./Layout.module.scss";
 import favicon from "../public/favicon.ico";
+import { useState } from "react";
 import { useTranslation } from "next-i18next";
 export type LayoutProps = {
   children: React.ReactNode;
@@ -15,32 +16,85 @@ export type LayoutProps = {
 const Layout = ({ children, fullwidth = false }) => {
   const { t: seo } = useTranslation("seo");
   const { t } = useTranslation("common");
+  const [password, setPassword] = useState("");
+  const [showDashboard, setShowDashboard] = useState(false);
+  const [error, setError] = useState("");
+
+  const handlePasswordSubmit = () => {
+    const correctPassword = "ciao";
+    if (password === correctPassword) {
+      setShowDashboard(true);
+    } else {
+      setError("Password errata");
+    }
+  };
 
   return (
     <>
-      <Head>
-        <title>{seo("title")}</title>
-        <meta name="description" content={seo("description")} />
-        <meta name="keywords" content={seo("keywords")} />
-        <meta name="author" content={seo("author")} />
-        {/** favicon */}
-        <link rel="icon" href={favicon.src} />
-
-        <NextSeo title={seo("title")} description={seo("description")} />
-      </Head>
-      <div>
-        <Header title={t("header.top.title")} tag={t("header.top.tag")} />
-        <div
-          className={classNames(
-            fullwidth ? "container-fluid" : "container",
-            fullwidth && classes.fullwidth
-          )}
-        >
-          {children}
-        </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        {!showDashboard ? (
+          <div
+            style={{
+              width: "40%",
+              padding: "5%",
+              border: "1px solid #660b0e",
+              borderRadius: "1.5%",
+              textAlign: "center",
+            }}
+          >
+            <h6>Inserisci la password per accedere</h6>
+            <input
+              style={{ border: "1px solid #660b0e", borderRadius: "2px" }}
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              style={{
+                background: "#660b0e",
+                color: "white",
+                borderRadius: "2px",
+              }}
+              onClick={handlePasswordSubmit}
+            >
+              Accedi
+            </button>
+            {error && <p style={{ color: "red" }}>{error}</p>}
+          </div>
+        ) : (
+          <div style={{ padding: 0, margin: 0 }}>
+            <Head>
+              <title>{seo("title")}</title>
+              <meta name="description" content={seo("description")} />
+              <meta name="keywords" content={seo("keywords")} />
+              <meta name="author" content={seo("author")} />
+              {/** favicon */}
+              <link rel="icon" href={favicon.src} />
+              <NextSeo title={seo("title")} description={seo("description")} />
+            </Head>
+            <div>
+              <Header title={t("header.top.title")} tag={t("header.top.tag")} />
+              <div
+                className={classNames(
+                  fullwidth ? "container-fluid" : "container",
+                  fullwidth && classes.fullwidth
+                )}
+              >
+                {children}
+              </div>
+            </div>
+            <Footer title={t("footer.title")} tag={t("footer.tag")} />
+            <Script src="/bootstrap-italia/dist/js/bootstrap-italia.bundle.min.js" />
+          </div>
+        )}
       </div>
-      <Footer title={t("footer.title")} tag={t("footer.tag")} />
-      <Script src="/bootstrap-italia/dist/js/bootstrap-italia.bundle.min.js" />
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,75 +3,20 @@ import { SUPERSET_DOMAIN, configProvider, dataProvider } from "../config";
 import { Dashboard } from "superset-dashboard-sdk";
 import Layout from "../components/Layout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useState } from "react";
 
 const Index = () => {
   const homeDashboard = configProvider.getHomePageDashboardConfig();
-  const [password, setPassword] = useState("");
-  const [showDashboard, setShowDashboard] = useState(false);
-  const [error, setError] = useState("");
-
-  const handlePasswordSubmit = () => {
-    const correctPassword = "AReSSits2023!";
-    if (password === correctPassword) {
-      setShowDashboard(true);
-    } else {
-      setError("Password errata");
-    }
-  };
-
   return (
     <Layout fullwidth>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        {!showDashboard ? (
-          <div
-            style={{
-              width: "300px",
-              padding: "20px",
-              border: "1px solid #660b0e",
-              borderRadius: "5px",
-              textAlign: "center",
-            }}
-          >
-            <h2>Inserisci la password per accedere</h2>
-            <input
-              style={{ border: "1px solid #660b0e", borderRadius: "2px" }}
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button
-              style={{
-                background: "#660b0e",
-                color: "white",
-                borderRadius: "2px",
-              }}
-              onClick={handlePasswordSubmit}
-            >
-              Accedi
-            </button>
-            {error && <p style={{ color: "red" }}>{error}</p>}
-          </div>
-        ) : (
-          <Dashboard
-            uuid={homeDashboard.uuid}
-            domain={SUPERSET_DOMAIN}
-            dataProvider={dataProvider}
-            fullheight
-          />
-        )}
-      </div>
+      <Dashboard
+        uuid={homeDashboard.uuid}
+        domain={SUPERSET_DOMAIN}
+        dataProvider={dataProvider}
+        fullheight
+      />
     </Layout>
   );
 };
-
 export const getServerSideProps = async ({ locale }) => {
   return {
     props: {
@@ -79,5 +24,4 @@ export const getServerSideProps = async ({ locale }) => {
     },
   };
 };
-
 export default Index;
