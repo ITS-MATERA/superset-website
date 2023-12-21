8ffa7d2a0c3ff544c59f68c577a8ef6bea985521
ITS-MATERA <eleonora.forcillo@itsmt.it>
2023-12-21 13:03:02 +0100

    dev: added access auth method with password

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,49 @@ import { SUPERSET_DOMAIN, configProvider, dataProvider } from "../config";
 import { Dashboard } from "superset-dashboard-sdk";
 import Layout from "../components/Layout";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useState } from "react"; // new line
 
 const Index = () => {
   const homeDashboard = configProvider.getHomePageDashboardConfig();
+  const [password, setPassword] = useState(""); // new line
+  const [showDashboard, setShowDashboard] = useState(false); // new line
+  const [error, setError] = useState(""); // new line
+
+  const handlePasswordSubmit = () => {
+    //new function
+    const correctPassword = "AReSSits2023!";
+    if (password === correctPassword) {
+      setShowDashboard(true);
+    } else {
+      setError("Password errata");
+    }
+  };
+
   return (
     <Layout fullwidth>
-      <Dashboard
-        uuid={homeDashboard.uuid}
-        domain={SUPERSET_DOMAIN}
-        dataProvider={dataProvider}
-        fullheight
-      />
+      {!showDashboard ? ( // new condition for true
+        <div>
+          <h2>Inserisci la password per accedere</h2>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handlePasswordSubmit}>Accedi</button>
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </div>
+      ) : (
+        <Dashboard
+          uuid={homeDashboard.uuid}
+          domain={SUPERSET_DOMAIN}
+          dataProvider={dataProvider}
+          fullheight
+        />
+      )}
     </Layout>
   );
 };
+
 export const getServerSideProps = async ({ locale }) => {
   return {
     props: {
@@ -24,4 +53,5 @@ export const getServerSideProps = async ({ locale }) => {
     },
   };
 };
+
 export default Index;
